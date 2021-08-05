9fba18ae0444eb5fbde0d20a651e571d670fdff1
Nikolas.Dev <62979208+Nikolas-as@users.noreply.github.com>
2021-08-05 16:59:39 -0300

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
+
+app.use(express.json());
 
 /**
  * Métodos HTTP:
@@ -11,131 +14,110 @@ const app = express();
  * DELETE: Deletar uma informação no back-end
  */
 
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Filtros e paginação 
+ * Routes Params: Identificar recursos (Atualizar/Delete)
+ * Request Body: Conteúdo na hora de criar e editar um recurso (JSON)
+ *  
+ */
+/**
+ * Middleware: 
+ * 
+ * Interceptador de requisições que interromper a requisição ou pode alterar dados da requisição
+ */
+
+const projects = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+
+  next(); //Faz o código continuar de onde parou. Next = próximo
+
+  console.timeEnd(logLabel)
+}
+
+function validadeProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'invalid project ID.' });
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validadeProjectId);
 
 app.get('/projects', (request, response) => {
-  return response.json({
-    Projetos: {
-      LetMeAsk: {
-        name: 'LetMeAsk',
-        date: '21/08/2021',
-        nota: 9,
-      },
-      IgNews: {
-        name: 'IgNews',
-        date: '06/02/2020',
-        nota: 10,
-      },
-      DtMoney: {
-        name: 'DtMoney',
-        date: '30/11/2021',
-        nota: 8,
-      },
-      FlexBlog: {
-        name: 'FlexBlog',
-        date: '21/04/2020',
-        nota: 7,
-      }
-    }
-  })
+  const { title } = request.query;
+
+  const results = title
+  ? projects.filter(project => project.title.includes(title))
+  : projects;
+
+  return response.json(results);
+
 })
 
-app.post('/projects/:id', (request, response) => {
-  return response.json({
-    Projetos: {
-      LetMeAsk: {
-        id: 1,
-        name: 'LetMeAsk',
-        date: '21/08/2021',
-        nota: 9,
-      },
-      IgNews: {
-        id: 2,
-        name: 'IgNews',
-        date: '06/02/2020',
-        nota: 10,
-      },
-      DtMoney: {
-        id: 3,
-        name: 'DtMoney',
-        date: '30/11/2021',
-        nota: 8,
-      },
-      FlexBlog: {
-        id: 4,
-        name: 'FlexBlog',
-        date: '21/04/2020',
-        nota: 7,
-      },
-      WildBeast: {
-        id: 5,
-        name: 'WildBeast',
-        date: '31/05/2020',
-        nota: 10,
-      }
-    }
-  })
+app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+
+  const project = { id: uuid(), title, owner };
+
+  projects.push(project);
+
+  return response.json(project);
+
 })
 
-app.put('/projects', (request, response) => {
-  return response.json({
-    Projetos: {
-      LetMeAsk: {
-        name: 'LetMeAsk',
-        date: '21/08/2021',
-        nota: 10,
-      },
-      IgNews: {
-        name: 'IgNews',
-        date: '06/02/2020',
-        nota: 10,
-      },
-      DtMoney: {
-        name: 'DtMoney',
-        date: '30/11/2021',
-        nota: 8,
-      },
-      FlexBlog: {
-        name: 'FlexBlog',
-        date: '21/04/2020',
-        nota: 7,
-      },
-      WildBeast: {
-        name: 'WildBeast',
-        date: '31/05/2020',
-        nota: 10,
-      }
-    }
-  })
-})
+app.put('/projects/:id', (request, response) => {
+
+  // const params = request.params;
+
+  // console.log(params)
+
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found!" })
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
+});
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json({
-    Projetos: {
-      LetMeAsk: {
-        name: 'LetMeAsk',
-        date: '21/08/2021',
-        nota: 9,
-      },
-      DtMoney: {
-        name: 'DtMoney',
-        date: '30/11/2021',
-        nota: 8,
-      },
-      FlexBlog: {
-        name: 'FlexBlog',
-        date: '21/04/2020',
-        nota: 7,
-      },
-      WildBeast: {
-        name: 'WildBeast',
-        date: '31/05/2020',
-        nota: 10,
-      }
-    }
-  })
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found!" })
+  }
+
+  projects.slice(projectIndex, 1);
+
+  return response.status(204).send();
 })
 
 app.listen(3333, () => {
-  return console.log('🚀 Back-end started!')
-});
+  return console.log('🚀 Back-end started!');
+})
 
